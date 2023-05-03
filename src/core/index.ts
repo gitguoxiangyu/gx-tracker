fd948e7c6a85518998b80b12d8e90b30c5802e70
gitguoxiangyu <3317755916@qq.com>
2023-05-03 21:56:20 +0800

    feat: merge default params and incoming params

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,13 +1,21 @@
 import { Option , SelectableOption } from '../types/index'
 
 export default class Tracker{
-  constructor(options : SelectableOption){
+  public baseData: SelectableOption;
 
+  constructor(options : SelectableOption){
+    // 将默认配置和传入配置合并
+    this.baseData = Object.assign(this.initDefault(),options)
   }
 
+  // 默认初始化baseData
   private initDefault (): Option{
     return <Option>{
-      
+      sdkVersion: '1.0.0',
+      historyTracker: false,
+      hashTracker: false,
+      domTracker: false,
+      jsError: false
     }
   }
 }
